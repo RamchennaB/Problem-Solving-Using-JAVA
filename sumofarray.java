88fa8273b6ecc03a8db5ec62314593b28719e0a9
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-03-12 18:08:17 +0530

    Create sumofarray.java

---
+++ sumofarray.java
@@ -0,0 +1,12 @@
+public class sumofarray {
+    public static void main(String []args){
+        int array[]=new int[]{1,2,3,4,5};;
+        int  sum=0;
+        for(int i=0;i<5;i++){
+            sum=sum+array[i];
+
+        }
+        System.out.println(sum);
+
+    }
+}
