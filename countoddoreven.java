22c8a890f80a4a7442121a8e2ca4c53ae84a38b4
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-03-20 20:33:34 +0530

    Update countoddoreven.java

--- countoddoreven.java
+++ countoddoreven.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class countoddoreven {
     public static void count(int arr[]) {
         int evencount = 0;
@@ -16,7 +17,12 @@ public class countoddoreven {
     }
 
     public static void main(String []args){
-        int arr[]=new int[]{2,4,6,8,10};;
+        Scanner s=new Scanne(System.in);
+        System.out.println("Enter the Size of an array");
+        int n=s.nextInt();
+        int arr[]=new int[n];
+        System.ou.println("Enter the Elements");
+        for(int i=0;i<n;i++) arr[i]=s.nextInt();
         count(arr);
     }
 }
