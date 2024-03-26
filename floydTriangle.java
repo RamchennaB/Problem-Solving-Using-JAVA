33183bcdcdaeadebc7f9faeefd1345091110ca08
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-03-26 22:16:18 +0530

    Update floydTriangle.java

--- floydTriangle.java
+++ floydTriangle.java
@@ -7,7 +7,7 @@ public class floydTriangle{
         n=s.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                System.out.print("*"+" ");
+                System.out.print("+"+" ");
                 
             }
             System.out.println();
