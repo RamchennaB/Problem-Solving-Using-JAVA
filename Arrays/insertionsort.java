6069ef77c4ed96a564e14e2fd9ae7074a7d77530
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-04-04 23:23:48 +0530

    Update insertionsort.java

--- Arrays/insertionsort.java
+++ Arrays/insertionsort.java
@@ -1,24 +1,30 @@
-class insertionsort{
-    public static void InsertionSort{
-       int n= arr.length;
-       for(int i=0;i<n-1;i++){
-           int key=arr[i];
-           int j=i-1;
-           while(j>=0 && arr[j] > key){
-               if(arr[j-1] > arr[j]){
-                   arr[j+1]=arr[j];
-                   j-=1;
-               }
-           }
-           arr[j-1]=key;
-       }
-   }
-   public static void main(String []args){
-       int n=5;
-       int arr[]=new int[]{5,7,6,8,4};;
-       InsertionSort(arr);
-       for(int i=0;i<n;i++){
-       System.out.print(arr[i]+ " ");
-       }
-   }
+class InsertionSort {
+    public static void insertionSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = new int[]{5, 7, 6, 8, 4};
+        System.out.println("Original Array:");
+        printArray(arr);
+        insertionSort(arr);
+        System.out.println("\nSorted Array:");
+        printArray(arr);
+    }
+
+    public static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
