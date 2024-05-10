94e74ff893c43af1eb6df630d34085152563d886
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-05-10 18:24:34 +0530

    Update Fibonnaci.java

--- Fibonnaci.java
+++ Fibonnaci.java
@@ -4,15 +4,15 @@ class Fibonacci{
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the length of the series");
         int n=sc.nextInt();
-        int a=0;
-        int b=1;
-        System.out.print(a+" "+b);
-        int c=0;
+        int f1=0;
+        int f2=1;
+        System.out.print(f1+" "+f2);
+        int f3=0;
         for(int i=0;i<n;i++){
-            c=a+b;
-            System.out.print(" "+c);
-            a=b;
-            b=c;
+            f3=f1+f2;
+            System.out.print(" "+f3);
+            f1=f2;
+            f2=f3;
         }
     }
 }
