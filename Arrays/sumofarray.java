688b8ac3e2edc5b5922fbf7066309a6b78270df7
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-05-10 18:25:13 +0530

    Update sumofarray.java

--- Arrays/sumofarray.java
+++ Arrays/sumofarray.java
@@ -1,9 +1,9 @@
 public class sumofarray {
     public static void main(String []args){
-        int array[]=new int[]{1,2,3,4,5};;
+        int arr[]=new int[]{1,2,3,4,5};;
         int  sum=0;
         for(int i=0;i<5;i++){
-            sum=sum+array[i];
+            sum=sum+arr[i];
 
         }
         System.out.println(sum);
