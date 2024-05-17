59c994e61113a91335e91023f0bb944829c0afa2
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-05-17 17:15:37 +0530

    Update SinglyLLAddoperations.java

--- LinkedList/SinglyLLAddoperations.java
+++ LinkedList/SinglyLLAddoperations.java
@@ -72,9 +72,9 @@ public static void main(String []args){
     singlyLinkedList s=new singlyLinkedList();
     s.addAtBeg(1);
     s.addAtBeg(2);
-    s.addAtPos(3,2);
     s.addAtEnd(0);
     s.addAtEnd(7);
+    s.addAtPos(3,2);
     s.display();
 }
 }
