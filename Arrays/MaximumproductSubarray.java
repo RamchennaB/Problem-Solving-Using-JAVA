714024e1cf7e7b5bb299e8cf8a66d91db4931c55
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-12-24 16:34:34 +0530

    Create MaximumproductSubarray.java

---
+++ Arrays/MaximumproductSubarray.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n=nums.length;
+        int maxprod=nums[0];
+        int minprod=nums[0];
+        int result=nums[0];
+        
+        for(int i=1;i<n;i++){
+            if(nums[i] < 0){
+            int temp=maxprod;
+            maxprod=minprod;
+            minprod=temp;
+           }
+            maxprod=Math.max(nums[i], maxprod * nums[i]);
+            minprod=Math.min(nums[i], minprod * nums[i]);
+
+            result=Math.max(result, maxprod);
+            
+         }
+         return result; 
+}
+}
